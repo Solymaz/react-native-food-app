5d9f5e2f873010a7a98fdd3505d74f5d5fd9b703
Solmaz <solmaz.mousavie@gmail.com>
2023-03-10 12:30:27 +0100

    search bar component

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import SearchBar from "../components/SearchBar";
+
+export default function SearchScreen() {
+  return (
+    <View>
+      <SearchBar />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
